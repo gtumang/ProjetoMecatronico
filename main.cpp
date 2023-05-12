13acc087d0b614002289bf48c56b1b423a7330c1
gtumang <gabrielt3@al.insper.edu.br>
2023-05-12 21:52:33 +0000

    adicionado rotina de guardar pontos de coleta e solta

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ InterruptIn chave_fdc_zdwn(FDC_ZDWN);
 InterruptIn btn_zup(BTN_ZUP);
 InterruptIn btn_zdwn(BTN_ZDWN);
 InterruptIn emergencia(EMER_2);
-InterruptIn btn_enter(BTN_ENTER);
-InterruptIn btn_cancel(BTN_CANCEL);
+DigitalIn btn_enter(BTN_ENTER);
+DigitalIn btn_cancel(BTN_CANCEL);
 
 DigitalOut MP_Z(MOTOR_Z);
 DigitalOut MP_Y(MOTOR_Y);
@@ -39,12 +39,11 @@ volatile bool enter_anterior = 0;
 volatile bool cancel = 0;
 volatile bool cancel_anterior = 0;
 volatile bool referenciado = 0;
-volatile short criar_posicao;
+int ponto_coleta[3];
+int pontos_solta[9][3];
 float passo_fuso = 1.8; //mm
 float passo_motor = 1.8; //graus
 float k = passo_fuso/(360.0/passo_motor); 
-
-int posicoes[2][9];
 
 //-----------------------------------------------Funcoes--------------------------------------------------------------//
 void pulsos_z(){
@@ -115,12 +114,6 @@ void retomar_emergencia(){
     en_all = 1;
 }
 
-void confirma(){
-    enter=!enter_anterior;
-}
-void cancela(){
-    cancel=!cancel_anterior;
-}
 
 void referenciar(){
     LCD_IHM.cls();
@@ -134,8 +127,105 @@ void referenciar(){
     }
     conta_passo = 0;
     referenciado = 1;
-    criar_posicao = 0;
+}
+
+void criar_coleta(){
+    bool criando_coleta=true;
+    bool checando;
     LCD_IHM.cls();
+    wait(0.2);
+    while(criando_coleta && en_all){
+        LCD_IHM.locate(1,0);
+        LCD_IHM.printf("Posicione no ponto");
+        LCD_IHM.locate(4,1);
+        LCD_IHM.printf("de coleta e clique");
+        LCD_IHM.locate(7,2);
+        LCD_IHM.printf("enter");
+        if(!btn_enter){
+            btn_zup.fall(NULL);
+            btn_zup.rise(NULL);
+            btn_zdwn.fall(NULL);
+            btn_zdwn.rise(NULL);
+            checando = true;
+            LCD_IHM.cls();
+            while(checando && en_all){
+                LCD_IHM.locate(0,0);
+                LCD_IHM.printf("Tem certeza?");
+                wait(0.2);
+                if(!btn_enter){
+                    ponto_coleta[0]=0;
+                    ponto_coleta[1]=0;
+                    ponto_coleta[2]=conta_passo;
+                    checando = false;
+                    criando_coleta = false;
+                }
+                if(!btn_cancel){
+                    checando=false;
+                }
+            }
+        }
+    }
+    btn_zup.fall(&movimentar_zup);
+    btn_zup.rise(&parar_zup);
+    btn_zdwn.fall(&movimentar_zdwn);
+    btn_zdwn.rise(&parar_zdwn);
+}
+
+void movimenta_solta(){
+    wait(0.2);
+    btn_zup.fall(&movimentar_zup);
+    btn_zup.rise(&parar_zup);
+    btn_zdwn.fall(&movimentar_zdwn);
+    btn_zdwn.rise(&parar_zdwn);
+    while(en_all){
+            LCD_IHM.locate(0,0);
+            LCD_IHM.printf("Posicione no ponto");
+            LCD_IHM.locate(2,1);
+            LCD_IHM.printf("de solta e clique");
+            LCD_IHM.locate(7,2);
+            LCD_IHM.printf("enter");
+            if(!btn_enter){
+                break;
+            }
+        }
+}
+
+bool checa_ponto(){
+    LCD_IHM.cls();
+    wait(0.2);
+    btn_zup.fall(NULL);
+    btn_zup.rise(NULL);
+    btn_zdwn.fall(NULL);
+    btn_zdwn.rise(NULL);
+    while(en_all){
+        LCD_IHM.locate(0,0);
+        LCD_IHM.printf("Tem certeza?");
+        if(!btn_enter){
+            return true;
+        }
+        if(!btn_cancel){
+            return false;
+        }
+    }
+}
+
+void criar_solta(){
+    int i=0;
+    while(i<10){
+        movimenta_solta();
+        if(checa_ponto()){
+            pontos_solta[i][0]=0;
+            pontos_solta[i][1]=0;
+            pontos_solta[i][2]=conta_passo;
+            i++;
+            LCD_IHM.cls();
+            LCD_IHM.locate(0,0);
+            LCD_IHM.printf("Ponto %i criado", i);
+            wait(1);
+
+        }
+    }
+
     btn_zup.fall(&movimentar_zup);
     btn_zup.rise(&parar_zup);
     btn_zdwn.fall(&movimentar_zdwn);
@@ -151,35 +241,89 @@ int main()
     chave_fdc_zdwn.rise(&liberar_zdwn);
     emergencia.fall(&parada_emergencia);
     emergencia.rise(&retomar_emergencia);
-    btn_enter.fall(&confirma);
-    btn_cancel.fall(&cancela);
+
+    int estado = 1;
+    int i =0;
 
     while (true) {
+        //------emergencia-------//
         if(!en_all){
             LCD_IHM.locate(0,0);
             LCD_IHM.printf("EMERGENCIA!    ");
             
             // printf("EMERGENCIA!");
-        }else if(!referenciado){
-            while(enter==enter_anterior){
+        }
+        //-----maquina de estados--------//
+        switch(estado){
+            //-------Referenciar-------//
+            case 1:{
+            while(btn_enter && en_all){
                 LCD_IHM.locate(7,0);
                 LCD_IHM.printf("Deseja");
                 LCD_IHM.locate(4,1);
                 LCD_IHM.printf("Referenciar?");
             }
+            enter_anterior = enter;
             referenciar();
-        }
-        else{
-        LCD_IHM.locate(0,0);
-        LCD_IHM.printf("Z: %1.2f mm    ",conta_passo*k);
-        LCD_IHM.locate(0,1);
-        LCD_IHM.printf("Passos: %i       ",abs(conta_passo));
-        }
-        // LCD_IHM.cls();
-        printf("\n\r%i",posicoes[0][0]);
-        for(int x; x<9; x++){
-            printf("\n\r%i",posicoes[1][x]);
+            estado = 2;
+            break;
+            }
+
+            //-----criar ponto de coleta--------//
+            case 2:{
+            bool perguntando = true;
+            LCD_IHM.cls();
+            while(perguntando && en_all){
+                LCD_IHM.locate(2,0);
+                LCD_IHM.printf("Deseja criar as     ");
+                LCD_IHM.locate(5,1);
+                LCD_IHM.printf("posicoes?      ");
+                if(!btn_enter){
+                    btn_zup.fall(&movimentar_zup);
+                    btn_zup.rise(&parar_zup);
+                    btn_zdwn.fall(&movimentar_zdwn);
+                    btn_zdwn.rise(&parar_zdwn);
+                    estado = 3;
+                    perguntando = false;
+                    
+                }
+                if(!btn_cancel){
+                    perguntando = false;
+                    estado = 4;
+                }
+            }
+            LCD_IHM.cls();
+            break;
+            }
+
+            case 3:{
+                criar_coleta();
+                estado = 4;
+            }
+            case 4:{
+                criar_solta();
+                estado=5;
+            }
+            case 5:{
+                if(i>9){
+                    i=0;
+                }
+                LCD_IHM.locate(0,0);
+                LCD_IHM.printf("Coleta       Solta %i ",i);
+                LCD_IHM.locate(0,1);
+                LCD_IHM.printf("X: %1.2f", ponto_coleta[0]*k);
+                LCD_IHM.printf("       X: %1.2f", pontos_solta[i][0]*k);
+                LCD_IHM.locate(0,2);
+                LCD_IHM.printf("Y: %1.2f", ponto_coleta[1]*k);
+                LCD_IHM.printf("       Y: %1.2f", pontos_solta[i][1]*k);
+                LCD_IHM.locate(0,3);
+                LCD_IHM.printf("Z: %1.2f", ponto_coleta[2]*k);
+                LCD_IHM.printf("       Z: %1.2f", pontos_solta[i][2]*k);
+                wait(2);
+                i++;
+            }
+            
+
         }
     }
-    enter_anterior = enter;
 }
